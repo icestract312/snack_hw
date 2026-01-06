dbe2aa2e8b27d4ad1f20526b28fcf61316a500f7
Aorakin <kingoak11@gmail.com>
2026-01-06 13:34:20 +0700

    feat: Update CORS middleware to allow additional origins

--- snack_web/app/main.py
+++ snack_web/app/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.database import engine, Base
@@ -17,6 +18,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        os.getenv("CORE_API_URL", ""),
     ],
     allow_methods=["*"],
     allow_headers=["*"],
